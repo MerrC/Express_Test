d08aa03acba538364db60bd5521f140bd8eb9d15
MerrC <32844325+MerrC@users.noreply.github.com>
2017-10-16 10:10:45 -0500

    Add Socket listener

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
-
+//Requirements
+var express = require('express');
+var fs      = require('fs');
+var mongojs = require('mongojs');
+var passport = require('passport')
+var util = require('util');
 var app = express();
+
+//App Creation
+var app = express();
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
 var server = app.listen(3000);
-var io = require('socket.io').listen(server); // this tells socket.io to use our express server
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+var io = require('socket.io').listen(server, function () {
+  console.log('Example io listening on port 3000!')
 });
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-
-
-console.log("Express server listening on port 3000");
