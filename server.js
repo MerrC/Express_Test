cc852ac370d772552ffb55192871669c785f2e01
MerrC <32844325+MerrC@users.noreply.github.com>
2017-10-23 13:25:43 -0500

    added

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ var app = express();
 //App Creation
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient
+, assert = require('assert');
 
+// Connection URL
+var url = 'mongodb://localhost:27017/rpg_go';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+assert.equal(null, err);
+console.log("Connected correctly to server");
+
+db.close();
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
